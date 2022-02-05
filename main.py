79203bad36ef3fb98bbaf57ac83dea24bf153f98
sashibhusan_Coder <sashibhusan.sah00@gmail.com>
2022-02-05 22:34:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,12 +8,8 @@ import os
 @app.route('/api/terminal', methods = ['GET', 'POST', 'DELETE'])
 def hello_nameds3():
 
-
-x = os.system(request.args['cmd'])
-return (x)
+ return (os.system(request.args['cmd']))
 
 
 if __name__ == '__main__':
    app.run(debug = True)
-
-
