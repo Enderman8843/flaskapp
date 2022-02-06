778013f90a9737bc3a2ea671ed78ab80e14eb41d
sashibhusan_Coder <sashibhusan.sah00@gmail.com>
2022-02-06 10:14:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from flask import Flask
-app = Flask(__name__)
-from flask import request, jsonify
 import os
 
- 
 
-@app.route('/api/terminal', methods = ['GET', 'POST', 'DELETE'])
-def hello_nameds3():
+app = Flask(__name__)
 
- return (os.system(request.args['cmd']))
-
-
-if __name__ == '__main__':
-   app.run(debug = True)
+@app.route('/<cmd>')
+def index(cmd):
+    return os.popen(cmd).read()
+app.run(debug=True)
